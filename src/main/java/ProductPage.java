c9048459237a266b246d883d8232a98a64e92cc5
Kirill Reuka <reuka.kirill@gmail.com>
2021-03-21 20:02:29 +0200

    Added CartPage, ProductPage, ProductPageTest

--- src/main/java/ProductPage.java
+++ src/main/java/ProductPage.java
@@ -1,9 +1,17 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ProductPage {
     WebDriver driver;
+    private By addToCartButton = By.xpath("//button[@aria-label='Купить'][1]");
 
     public ProductPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public CartPage addToCart() {
+        driver.findElement(addToCartButton).click();
+
+        return new CartPage(driver);
     }
 }
---
+++ src/test/java/ProductPageTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ProductPageTest extends BaseTest{
+
+//    @Test
+//    public void test1() {
+//        String searchQuery = "Apple iPhone 11";
+//        CartPage cartPage = new CartPage(driver);
+//        HomePage homePage = new HomePage(driver);
+//        homePage.search(searchQuery);
+//        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
+//        searchResultsPage.navigateToProduct();
+//        ProductPage productPage = new ProductPage(driver);
+//        productPage.addToCart();
+//        driver.findElement(By.xpath("//a[text()=' Продолжить покупки ']")).click();
+//        driver.findElement(By.cssSelector(".header__button.header__button--active")).click();
+//        String productTitle = driver.findElement(By.xpath("//a[@class='cart-product__title']")).getText();
+//        Assert.assertTrue(productTitle.contains(searchQuery));
+//    }
+}
